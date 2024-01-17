b3be4a8482a50706484857b073cb00a425feda6b
Khadija Elmoutouakil <kelmouto@e1r13p11.1337.ma>
2024-01-17 12:50:42 +0100

    push

--- WebServer.cpp
+++ WebServer.cpp
@@ -10,15 +10,6 @@ WebServer::WebServer(int ac, char **av)
         conf = "config.conf";
     std::string f = o.ReadFile(conf.c_str());
     servers = o.splitServers(f, 0);
-     std::map<std::string,Location>::iterator it = (servers.begin())->locations.begin();
-    for(;it != (servers.begin())->locations.end();it++)
-    {
-        std::cout<< "name : "<< it->first << " : ";
-        std::vector<std::string> ::iterator ite = it->second.index.begin();
-        for(;ite != it->second.index.end();ite++)
-            std::cout<<">"<< *ite << "< , ";
-        std::cout<< "\n";
-    }
-    exit(0);
+    std::map<std::string,Location>::iterator it = (servers.begin())->locations.begin();
     servers.begin()->funcMimeTypes("mime.types");
 }
